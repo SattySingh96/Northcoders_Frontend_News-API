288dda78fb8c2a0b6d4560ec613057caf2f52ecd
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-12-18 21:28:24 +0000

    Added Voter component to each ArticleCardC

--- northcoders---frontend---news-api/src/Components/ArticleCard.jsx
+++ northcoders---frontend---news-api/src/Components/ArticleCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "@reach/router";
+import Voter from "./Voter";
 
-const ArticleCard = ({ article_id, title }) => {
+const ArticleCard = ({ article_id, title, votes }) => {
   return (
-    <article className='ArticleCard'>
+    <article className="ArticleCard">
       <Link to={`/articles/${article_id}`}>
         <h2>{title}</h2>
       </Link>
+      <Voter id={article_id} type={"articles"} votes={votes} />
     </article>
   );
 };
