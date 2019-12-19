3b674127ac20f3c24599e29365c0f05366078166
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2019-12-19 11:48:56 +0000

    Deleted superfluous nested folders

---
+++ build/precache-manifest.aca371adc4b824f0e710859a0c4480b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c287b541e07180c0b6bb29183be34ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b746e43a40dad8d0a379",
+    "url": "/static/css/main.edf3a4e3.chunk.css"
+  },
+  {
+    "revision": "08840371840f1298d254",
+    "url": "/static/js/2.f65d0498.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.f65d0498.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b746e43a40dad8d0a379",
+    "url": "/static/js/main.302a2619.chunk.js"
+  },
+  {
+    "revision": "7f1c08a640e0a368894b",
+    "url": "/static/js/runtime-main.efd80f85.js"
+  }
+]);
