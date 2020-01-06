496412d694fd7c58d492ea891c736e82607d1b5d
sattysingh96 <satnam.bhatti.singh96@gmail.com>
2020-01-06 11:46:10 +0000

    Refactored styling off articleCard to make use of grid

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -12,25 +12,21 @@ const ArticleCard = ({
 }) => {
   return (
     <article className="ArticleCard">
+      <h2 id="ArticleAuthor">
+        Posted by {author} on {created_at.slice(0, 10)}
+      </h2>
 
-      <h2 id='ArticleAuthor'>Posted by {author} on {created_at.slice(0,10)}</h2>
-
-      <Link className="Link" to={`/articles/${article_id}`}>
+      <Link id="ArticleTitle" to={`/articles/${article_id}`}>
         <h2>{title}</h2>
-      </Link>      
+      </Link>
 
-      <h2 id="ArticleComments">Comments: {comment_count}</h2>
+      <h2 id="ArticleCommentsValue">Comments: {comment_count}</h2>
 
-      <div className='ArticleCardVoter'>
-
-        <Voter        
-        id={article_id}
-        type={"articles"}
-        votes={votes}
-      /> 
-
-      </div>  
-              
+      <div id="ColorBlock1"></div>
+      <div id="ColorBlock2"></div>
+      <div id="ArticleCardVoter">
+        <Voter id={article_id} type={"articles"} votes={votes} />
+      </div>
     </article>
   );
 };
